3bf1e473db1e3dacb488674c8f731ac08925bdc5
Domizio Demichelis <dd.nexus@gmail.com>
2013-01-28 12:49:09 +0100

    fixes the syncer error on syncing self

--- lib/flex/class_proxy/model_syncer.rb
+++ lib/flex/class_proxy/model_syncer.rb
@@ -5,12 +5,10 @@ module Flex
       attr_accessor :synced
 
       def sync(*synced)
-
-        # Flex::ActiveModel has its own way of syncing, and a Flex::ModelSyncer only cannot be synced
-        if synced.any?{|s| s == context} && context.include?(Flex::ActiveModel) || !context.include?(Flex::ModelMapper)
-          raise ArgumentError, %(You cannot flex.sync(self) #{self}.)
+        # Flex::ActiveModel has its own way of syncing, and a Flex::ModelSyncer cannot be synced by itself
+        if synced.any?{|s| s == context} && (context.include?(Flex::ActiveModel) || !context.include?(Flex::ModelMapper))
+          raise ArgumentError, %(You cannot flex.sync(self) #{context}.)
         end
-
         @synced = synced
         context.class_eval do
           raise NotImplementedError, "the class #{self} must implement :after_save and :after_destroy callbacks" \
