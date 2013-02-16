28cc004299aa666bc3f0e516db84d9150c74b3e8
Domizio Demichelis <dd.nexus@gmail.com>
2013-02-16 21:40:09 +0100

    small cleanup in ActiveModel::Inspection

--- lib/flex/active_model/inspection.rb
+++ lib/flex/active_model/inspection.rb
@@ -3,17 +3,17 @@ module Flex
     module Inspection
 
       def inspect
-        full_descriptions = [%(_id: #{@_id.inspect}), %(_version: #{@_version})]
-        all_attributes    = if respond_to?(:raw_document)
-                              reader_keys = raw_document.send(:readers).keys
-                              # we send() the readers, so they would reflect an eventual overriding
-                              Hash[ reader_keys.map{ |k| [k, send(k)] } ].merge(attributes)
-                            else
-                              attributes
-                            end
-        full_descriptions << Utils.keyfy(:to_sym, all_attributes).sort.map { |key, value| "#{key}: #{value.inspect}" }
-        separator         = " " unless full_descriptions.empty?
-        "#<#{self.class.name}#{separator}#{full_descriptions.join(", ")}>"
+        descriptions   = [%(_id: #{@_id.inspect}), %(_version: #{@_version})]
+        all_attributes = if respond_to?(:raw_document)
+                           reader_keys = raw_document.send(:readers).keys.map(&:to_s)
+                           # we send() the readers, so they will reflect an eventual overriding
+                           Hash[ reader_keys.map{ |k| [k, send(k)] } ].merge(attributes)
+                         else
+                           attributes
+                         end
+        descriptions << all_attributes.sort.map { |key, value| "#{key}: #{value.inspect}" }
+        separator = " " unless descriptions.empty?
+        "#<#{self.class.name}#{separator}#{descriptions.join(", ")}>"
       end
 
     end
