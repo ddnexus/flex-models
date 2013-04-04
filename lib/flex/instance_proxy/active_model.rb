ad7df0f8825baa6e887b9919f8e2a847085ce38f
Domizio Demichelis <dd.nexus@gmail.com>
2013-04-04 11:31:49 +0200

    added the ability to generate the id for the new records in the model

--- lib/flex/instance_proxy/active_model.rb
+++ lib/flex/instance_proxy/active_model.rb
@@ -4,8 +4,12 @@ module Flex
 
       def store(*vars)
         return super unless instance.flex_indexable? # this should never happen since flex_indexable? returns true
-        meth = instance.new_record? ? :post_store : :store
+        meth = instance.respond_to?(:new_record_id) || !instance.new_record? ? :store : :post_store
         Flex.send(meth, metainfo, {:data => instance.flex_source}, *vars)
+      end
+
+      def id
+        instance.new_record? && instance.respond_to?(:new_record_id) ? instance.new_record_id : super
       end
 
     end
