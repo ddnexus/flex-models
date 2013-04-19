ec6ae844855d8d57e693befdbc1b8d05ff3d5ece
Domizio Demichelis <dd.nexus@gmail.com>
2013-04-19 17:04:05 +0200

    renamed new_record_id > generate_id

--- lib/flex/instance_proxy/active_model.rb
+++ lib/flex/instance_proxy/active_model.rb
@@ -4,12 +4,12 @@ module Flex
 
       def store(*vars)
         return super unless instance.flex_indexable? # this should never happen since flex_indexable? returns true
-        meth = instance.respond_to?(:new_record_id) || !instance.new_record? ? :store : :post_store
+        meth = instance.respond_to?(:generate_id) || !instance.new_record? ? :store : :post_store
         Flex.send(meth, metainfo, {:data => instance.flex_source}, *vars)
       end
 
       def id
-        instance.new_record? && instance.respond_to?(:new_record_id) ? instance.new_record_id : super
+        instance.new_record? && instance.respond_to?(:generate_id) ? instance.generate_id : super
       end
 
     end
