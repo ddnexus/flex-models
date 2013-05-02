88a10384bffe780eb6c4f715ec6c19de9920f2cd
Domizio Demichelis <dd.nexus@gmail.com>
2013-05-02 23:20:32 +0200

    fix for method_missing

--- lib/flex/result/active_model.rb
+++ lib/flex/result/active_model.rb
@@ -20,7 +20,7 @@ module Flex
           class << res; self end.class_eval do
             define_method(:raw_result){ raw_result }
             define_method(:method_missing) do |meth, *args, &block|
-              super(*args, &block) unless raw_result.respond_to?(meth)
+              super(meth, *args, &block) unless raw_result.respond_to?(meth)
               raw_result.send(meth, *args, &block)
             end
           end
@@ -44,7 +44,7 @@ module Flex
               doc.respond_to?(*args) || super(*args)
             end
             define_method(:method_missing) do |meth, *args, &block|
-              super(*args, &block) unless respond_to?(meth)
+              super(meth, *args, &block) unless respond_to?(meth)
               doc.send(meth, *args, &block)
             end          end
           @_id        = doc['_id']
